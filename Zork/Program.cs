95d4bc1db72102cf8a4e610bea8f3d15f250fac9
kriszcvc <59684725+kriszcvc@users.noreply.github.com>
2021-09-15 22:33:59 -0400

    Zork 2.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -4,12 +4,100 @@ namespace Zork
 {
     class Program
     {
+        private static string Location
+        {
+            get
+            {
+                return Rooms[LocationColumn];
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write($"{Rooms[LocationColumn]}\n\n>");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                        outputString = "The way is shut!";
+                        break;
+
+                    case Commands.EAST:
+                        if (LocationColumn < Rooms.Length - 1)
+                        {
+                            LocationColumn++;
+                            outputString = $"You moved {command}.";
+                        }
+                        else
+                        {
+                            outputString = "The way is shut!";
+                        }
+                        break;
+
+                    case Commands.WEST:
+                        if (LocationColumn > 0)
+                        {
+                            LocationColumn--;
+                            outputString = $"You moved {command}.";
+                        }
+                        else
+                        {
+                            outputString = "The way is shut!";
+                        }
+                        break;
+
+                    default:
+                        outputString = "Unrecognized command.";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            }
+        }
+
+        private static bool Move(Commands command)
+        {
+            bool didMove = false;
+
+            switch (command)
+            {
+                case Commands.NORTH:
+                case Commands.SOUTH:
+                    break;
+
+                case Commands.EAST:
+                    if (LocationColumn < Rooms.Length - 1)
+                    {
+                        LocationColumn++;
+                        didMove = true;
+                    }
+                    break;
+
+                case Commands.WEST:
+                    if (LocationColumn > 0)
+                    {
+
+                    }
+                    break;
+            }
+
+            return didMove;
         }
 
         private static Commands ToCommand(string commandString)
@@ -24,5 +112,9 @@ namespace Zork
                 return Commands.UNKNOWN;
             }
         }
+
+        private static string[] Rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
+
+        private static int LocationColumn = 1;
     }
 }
